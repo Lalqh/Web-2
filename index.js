361ce022e40177e701f0105691ca53b198919b5b
Lalqh <lalincotero13@gmail.com>
2024-02-12 12:41:10 -0600

    Actvidad de ejemplo del servidor

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const app = express();
+
+app.use(express.json());
+
+let games = [
+{id: 1, name: 'Mario Wonder'},
+{id: 2, name: 'SpiderMan'},
+{id: 3, name: 'Left 4 dead'}]
+
+app.get("/games", (req, res) =>{
+    res.send(games)
+});
+
+app.post("/games", (req, res) => {
+    const { body }  = req
+    games.push(body)
+    res.send("Se agrego el juego")
+});
+
+app.patch("/games:id", (req, res) => {
+    const { body }  = req
+    const { name } = body
+    const { params } = req.params
+    const { id } = params
+    let game = games.find((game) => game.id == id)
+    game.name = name
+    res.send({message: "se elimino el juego", game})
+});
+
+app.delete("/games:id", (req, res) => {
+    const { body }  = req
+    const { name } = body
+    const { params } = req.params
+    const { id } = params
+    let game = games.filter((game) => game.id != id)
+    res.send("Se elimino el juego")
+});
+
+app.listen(3000)
